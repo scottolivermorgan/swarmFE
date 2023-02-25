fd75226e00382710cb83959528347b8b35ddf9cf
Scott Morgan <scottolivermorgan@yahoo.com>
2023-02-25 13:32:33 +0000

    added git link in footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -10,11 +10,18 @@ function Footer() {
 			<div className="row" align="center">
 			<br></br>
 				<center>
-					<SocialIcon bgColor="white" url="https://www.facebook.com/Swarm-Labs-102522568202848" /> {' '}
-					<SocialIcon bgColor="white" url="https://twitter.com/SwarmLtd" /> {' '}
-					<SocialIcon bgColor="white" url="https://www.linkedin.com/in/chris-lowe-1a63881b2/" /> {' '}
-					<SocialIcon bgColor="white" url="https://www.instagram.com/swarmlabs" /> {' '}
-					<SocialIcon bgColor="white" url="https://www.youtube.com/channel/UC4xdb2kD3QfE5TkDHT8x71w/featured" />  {' '}
+					<SocialIcon bgColor="white" style={{ height: 25, width: 25 }}
+						 url="https://www.facebook.com/Swarm-Labs-102522568202848" /> {'s'}
+					<SocialIcon bgColor="white" style={{ height: 25, width: 25 }} 
+						url="https://twitter.com/SwarmLtd" /> {'s'}
+					<SocialIcon bgColor="white" style={{ height: 25, width: 25 }} 
+						url="https://www.linkedin.com/in/chris-lowe-1a63881b2/" /> {'s'}
+					<SocialIcon bgColor="white" style={{ height: 25, width: 25 }} 
+						url="https://www.instagram.com/swarmlabs" /> {'s'}
+					<SocialIcon bgColor="white" style={{ height: 25, width: 25 }} 
+						url="https://www.youtube.com/channel/UC4xdb2kD3QfE5TkDHT8x71w/featured" />  {'s'}
+					<SocialIcon bgColor="white" style={{ height: 25, width: 25 }} 
+						url="https://github.com/scottolivermorgan?tab=repositories" /> {'s'}
                 </center>
 			</div>
 			<sub className="copy-right">© 2023 Copyright</sub>
