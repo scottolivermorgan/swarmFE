b59392db33277b14c860c691df52c52dcda30fc4
Scott Morgan <scottolivermorgan@yahoo.com>
2023-02-25 11:36:38 +0000

    added logo to landing

--- src/Components/Landing/Landing.js
+++ src/Components/Landing/Landing.js
@@ -1,10 +1,17 @@
 import React from 'react';
+import logo from '../../images/logo.png';
+import "./Landing.css";
 //import Button from 'react-bootstrap/Button';
 //import { Link } from 'react-router-dom';
 
+console.log(logo)
 function Landing() {
     return(
-        <div>Land</div>
+        <div>
+            <center className="logo-box">
+            <img src = {logo} className="App-logo" alt="logo"></img>
+            </center>
+        </div>
     )
 }
 export default Landing;
