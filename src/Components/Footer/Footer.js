a5fa2955d03c46e045dfd02ec7908040fd31ceb0
Scott Morgan <scottolivermorgan@yahoo.com>
2023-02-25 10:27:23 +0000

    Formated footer

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { SocialIcon } from 'react-social-icons';
+import "./Footer.css";
 
 function Footer() {
     return(
-        <footer className="page-footer font-small " align="center">
+        <footer className="page-footer" align="center">
 		<div className="container">
 		<center>
 			<div className="row" align="center">
+			<br></br>
 				<center>
-					<SocialIcon url="https://www.facebook.com/Swarm-Labs-102522568202848" />
-					<SocialIcon url="https://twitter.com/SwarmLtd" />
-					<SocialIcon url="https://www.linkedin.com/in/chris-lowe-1a63881b2/" />
-					<SocialIcon url="https://www.instagram.com/swarmlabs" />
-					<SocialIcon url="https://www.youtube.com/channel/UC4xdb2kD3QfE5TkDHT8x71w/featured" />
+					<SocialIcon bgColor="white" url="https://www.facebook.com/Swarm-Labs-102522568202848" /> {' '}
+					<SocialIcon bgColor="white" url="https://twitter.com/SwarmLtd" /> {' '}
+					<SocialIcon bgColor="white" url="https://www.linkedin.com/in/chris-lowe-1a63881b2/" /> {' '}
+					<SocialIcon bgColor="white" url="https://www.instagram.com/swarmlabs" /> {' '}
+					<SocialIcon bgColor="white" url="https://www.youtube.com/channel/UC4xdb2kD3QfE5TkDHT8x71w/featured" />  {' '}
                 </center>
 			</div>
-			<br></br>
-			<sub>© 2022 Copyright</sub>
-			<sub > Swarm Labs</sub>
+			<sub className="copy-right">© 2023 Copyright</sub>
+			<sub className="copy-right"> Swarm Labs</sub>
 		</center>
 		</div>
 	</footer>
