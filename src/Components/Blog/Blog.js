e50c73cd1118d888a71c2809b4eca8871fdea9a4
Scott Morgan <scottolivermorgan@yahoo.com>
2023-10-29 15:56:01 +0000

    Blog media cards added.

--- src/Components/Blog/Blog.js
+++ src/Components/Blog/Blog.js
@@ -2,26 +2,136 @@ import React from 'react';
 import logo from '../../images/logo.png';
 import { Link } from 'react-router-dom';
 import './Blog.css'
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+import SideNav from '../navbar/navbar.js';
+import { CardActionArea } from '@mui/material';
+import temp from '../../images/temp.png';
+import '../navbar/navbar.css'
 
 function Blog() {
+	const BASE_URL = process.env.REACT_BASE_APP_URL;
     return(
         <>
-		<div className="logo-box-sm">
-        <img src = {logo} className="App-logo-sm" alt="logo"></img>
-        </div>
-		<center className="blurb-box">
-		<div className="title"> <b>Blog</b></div>		
-	    <div>
-        Duis aute irure dolor in 
-		reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-		pariatur. Excepteur sint occaecat cupidatat non proident, sunt in 
-		culpa qui officia deserunt mollit anim id est laborum.
-		</div>
-		<br></br>
-		<Link to='/'> 
-                    <button className = "main-btn">Home</button>
-                </Link>
-		</center>
+		<table>
+			<th>
+				{SideNav()}
+			</th>
+			<th>
+				<td>
+				<center className="blurb-box">
+					<div className="about-us"> <b>Blog Posts</b></div>		
+	    				<div>
+							
+						</div>
+						<br></br>
+						<div class='container'>
+        	<table>
+				<tr>
+					<td>
+						<Card sx={{ maxWidth: 1145 }}>
+						<CardActionArea>
+      						<CardMedia sx={{ height: 340 }} image={temp} 
+													title="Scott"/>
+      						<CardContent>
+        						<Typography gutterBottom variant="h5" component="div">
+									<center>
+										<b>Post Title</b>
+									</center>
+								
+        						</Typography>
+        						<Typography variant="body2" color="text.secondary">
+									<center>
+										blurb about post
+									</center>
+        						</Typography>
+								<center>
+								</center>
+      						</CardContent>
+							</CardActionArea>
+    					</Card>
+					</td>
+					<td>
+						<Card sx={{ maxWidth: 345 }}>
+						<CardActionArea>
+      						<CardMedia sx={{ height: 140 }} image={temp} 
+													title="Scott"/>
+      						<CardContent>
+        						<Typography gutterBottom variant="h5" component="div">
+									<center>
+										<b>Post Title</b>
+									</center>
+								
+        						</Typography>
+        						<Typography variant="body2" color="text.secondary">
+									<center>
+										blurb about post
+									</center>
+        						</Typography>
+								<center>
+								</center>
+      						</CardContent>
+							</CardActionArea>
+    					</Card>
+					</td>
+				</tr>
+				
+				<tr>
+					<td>
+						<Card sx={{ maxWidth: 345 }}>
+						<CardActionArea>
+      						<CardMedia sx={{ height: 140 }} image={temp} 
+													title="Scott"/>
+      						<CardContent>
+        						<Typography gutterBottom variant="h5" component="div">
+									<center>
+										<b>Post Title</b>
+									</center>
+								
+        						</Typography>
+        						<Typography variant="body2" color="text.secondary">
+									<center>
+										blurb about post
+									</center>
+        						</Typography>
+								<center>
+								</center>
+      						</CardContent>
+							</CardActionArea>
+    					</Card>
+					</td>
+					<td>
+						<Card sx={{ maxWidth: 345 }}>
+						<CardActionArea>
+      						<CardMedia sx={{ height: 140 }} image={temp} 
+													title="Scott"/>
+      						<CardContent>
+        						<Typography gutterBottom variant="h5" component="div">
+									<center>
+										<b>Post Title</b>
+									</center>
+								
+        						</Typography>
+        						<Typography variant="body2" color="text.secondary">
+									<center>
+										blurb about post
+									</center>
+        						</Typography>
+								<center>
+								</center>
+      						</CardContent>
+							</CardActionArea>
+    					</Card>
+					</td>
+				</tr>
+			</table>
+        	</div>
+			</center>
+			</td>
+			</th>
+			</table>
         </>
     )
 }
